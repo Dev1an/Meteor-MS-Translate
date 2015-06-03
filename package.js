12a75dafd5221812d396453a0beb63d1b9a24f9d
Devian <info@devian.tk>
2015-06-03 18:22:00 +0200

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'devian:mstranslate',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Tranlsate with Microsoft Translator',
   // URL to the Git repository containing the source code for this package.
