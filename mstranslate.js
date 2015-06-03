f96a07dc4e7989dd0cf2addc38d67ec5bf20fd87
Devian <info@devian.tk>
2015-06-03 18:05:30 +0200

    add detection method

--- mstranslate.js
+++ mstranslate.js
@@ -1,17 +1,25 @@
 Microsoft = {
-	translate: translate
+	translate: translate,
+	detect: detect
 }
 
-function translate(text, language) {
-	var response = HTTP.get('http://api.microsofttranslator.com/V2/Ajax.svc/Translate', {
-		params: {
-			text: text,
-			to: language,
-			contentType: 'text/plain',
-			appId: ''
-		},
+function request(path, params) {
+	var response = HTTP.get('http://api.microsofttranslator.com/V2/Ajax.svc/' + path, {
+		params: _.extend({ appId: ''}, params),
 		headers: getAuthorizationHeader()
 	})
 
 	return EJSON.parse(response.content)
+}
+
+function translate(text, language) {
+	return request('Translate', {
+		text: text,
+		to: language,
+		contentType: 'text/plain'
+	})
+}
+
+function detect(text) {
+	return request('Detect', { text: text })
 }
